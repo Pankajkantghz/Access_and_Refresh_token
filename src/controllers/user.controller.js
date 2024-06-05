646bf37599eb62bdfb567409a0b81935e3588b55
Pankaj Yadav <pankajkantnitian@gmail.com>
2024-06-05 11:38:57 +0530

    first commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,7 +15,7 @@ const  registerUser = asyncHandler(async (req,res) =>{
   //remove password and refresh token field from response
   //return res
   const {fullName, email,username,password}= req.body
-  console.log("email:",email);
+ // console.log("email:",email);
 
 
   if(
@@ -26,7 +26,7 @@ const  registerUser = asyncHandler(async (req,res) =>{
 
 
 
-  const existedUser = User.findOne({
+  const existedUser =await User.findOne({
     $or: [{ username },{ email }]
   })
 
@@ -36,7 +36,12 @@ const  registerUser = asyncHandler(async (req,res) =>{
 
 
  const avatarLocalPath = req.files?.avatar[0]?.path;
- const coverImageLocalPath = req.files?.coverImage[0]?.path;
+//  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+ let coverImageLocalPath ;
+ if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+  coverImageLocalPath=req.files.coverImage[0].path
+ }
+
 
  if(!avatarLocalPath){
   throw new ApiError(400,"Avatar file is required")
@@ -51,24 +56,25 @@ const  registerUser = asyncHandler(async (req,res) =>{
  }
 
 
- const user = awaitUser.create({
+ const user = await User.create({
+  fullName,
   avatar: avatar.url,
   coverImage:coverImage?.url || "",
   email,
   password,
   username:username.toLowerCase()
  })
- const createUser=await User.findById(user._id).select(
+ const createdUser=await User.findById(user._id).select(
   "-password -refreshToken"
  )
 
- if (!createUser) {
+ if (!createdUser) {
   throw new ApiError(500,"Something went wrong while registering the user")
  }
 
 
  return res.status(201).json(
-  new ApiResponse(200,"User registered Sucessfully")
+  new ApiResponse(200,createdUser,"User registered Sucessfully")
  )
 
 
